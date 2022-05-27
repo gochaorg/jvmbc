2415a2f798c693813667d0bee9e58e67392bb8c8
Kamnev Georgiy <nt.gocha@gmail.com>
2022-05-28 02:05:13 +0500

    comments

--- jvmbc-score3/jvmbc-score3/src/main/scala/xyz/cofe/jvmbc/fld/model.scala
+++ jvmbc-score3/jvmbc-score3/src/main/scala/xyz/cofe/jvmbc/fld/model.scala
@@ -5,5 +5,7 @@ import ann.AnnCode
 
 sealed trait FieldCode
 case class FAnnotation(desc:TDesc,visible:Boolean,annotations:Seq[AnnCode]) extends FieldCode
+
+/** end of the method */
 case class FieldEnd() extends FieldCode
 case class FTypeAnnotation(typeRef:Int,typePath:String,desc:TDesc,visible:Boolean) extends FieldCode
