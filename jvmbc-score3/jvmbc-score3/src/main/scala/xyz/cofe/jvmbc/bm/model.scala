764c2d749221b73f292310f2eca1d3894f53ab18
Georgiy Kamnev <nt.gocha@gmail.com>
2022-11-29 21:39:53 +0500

    bm toAsm

--- jvmbc-score3/jvmbc-score3/src/main/scala/xyz/cofe/jvmbc/bm/model.scala
+++ jvmbc-score3/jvmbc-score3/src/main/scala/xyz/cofe/jvmbc/bm/model.scala
@@ -1,20 +1,39 @@
 package xyz.cofe.jvmbc
 package bm
 
-sealed trait BootstrapArg
+sealed trait BootstrapArg:
+  def toAsm:Object
 
-case class Handle(tag:Int,desc:TDesc,name:String,owner:String,iface:Boolean) extends BootstrapArg
+case class Handle(tag:Int,desc:TDesc,name:String,owner:String,iface:Boolean) extends BootstrapArg:
+  override def toAsm: org.objectweb.asm.Handle = org.objectweb.asm.Handle(tag, owner, name, desc.raw, iface)
+
 object Handle:
   def apply( h:org.objectweb.asm.Handle ):Handle =
     Handle(h.getTag, TDesc(h.getDesc), h.getName, h.getOwner, h.isInterface)
 
-case class TypeArg(value:String) extends BootstrapArg
-case class StringArg(value:String) extends BootstrapArg
-case class LongArg(value:Long) extends BootstrapArg
-case class IntArg(value:Int) extends BootstrapArg
-case class FloatArg(value:Float) extends BootstrapArg
-case class DoubleArg(value:Double) extends BootstrapArg
-case class ConstDynamic(name:String,desc:String, handle: Handle, args:List[BootstrapArg]) extends BootstrapArg
+case class TypeArg(value:String) extends BootstrapArg:
+  override def toAsm: Object = org.objectweb.asm.Type.getType(value)
+
+case class StringArg(value:String) extends BootstrapArg:
+  override def toAsm: Object = value
+
+case class LongArg(value:Long) extends BootstrapArg:
+  override def toAsm: Object = java.lang.Long.valueOf(value)
+
+case class IntArg(value:Int) extends BootstrapArg:
+  override def toAsm: Object = java.lang.Integer.valueOf(value)
+
+case class FloatArg(value:Float) extends BootstrapArg:
+  override def toAsm: Object = java.lang.Float.valueOf(value)
+
+case class DoubleArg(value:Double) extends BootstrapArg:
+  override def toAsm: Object = java.lang.Double.valueOf(value)
+
+case class ConstDynamic(name:String,desc:String, handle: Handle, args:List[BootstrapArg]) extends BootstrapArg:
+  override def toAsm: Object = new org.objectweb.asm.ConstantDynamic(
+    name, desc, handle.toAsm, args.map(_.toAsm):_*
+  )
+
 object ConstDynamic:
   def apply(a:org.objectweb.asm.ConstantDynamic):Either[String,ConstDynamic] =
     (0 until a.getBootstrapMethodArgumentCount)
