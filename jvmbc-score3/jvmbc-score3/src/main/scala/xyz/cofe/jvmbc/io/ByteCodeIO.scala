a8d22837f45f0bcf4eb3232d8a283e4c8620cd6b
Kamnev Georgiy <nt.gocha@gmail.com>
2022-12-09 13:20:12 +0500

    use ByteCodeIO obj without trait

--- jvmbc-score3/jvmbc-score3/src/main/scala/xyz/cofe/jvmbc/io/ByteCodeIO.scala
+++ jvmbc-score3/jvmbc-score3/src/main/scala/xyz/cofe/jvmbc/io/ByteCodeIO.scala
@@ -3,18 +3,27 @@ package io
 
 import cls.CBegin
 import org.objectweb.asm.ClassReader
-
-trait ByteCodeIO[ERR] {
-  def parse(bytes:Array[Byte]):Either[ERR,CBegin]
-}
+import scala.util.Using
+import scala.util.Failure
+import scala.util.Success
 
 object ByteCodeIO {
-  def parser:ByteCodeIO[String]=
+  def parse(bytes:Array[Byte]):Either[String,CBegin] =
     val inst = ClassDump(org.objectweb.asm.Opcodes.ASM9)
-    new ByteCodeIO[String] {
-      override def parse(bytes:Array[Byte]):Either[String,CBegin]=
-        val cr = new ClassReader(bytes)
-        cr.accept(inst,0)
-        inst.build
-    }
+    val cr = new ClassReader(bytes)
+    cr.accept(inst,0)
+    inst.build
+
+  def parse(clazz:Class[_]):Either[String,CBegin] =
+    val resName = clazz.getName().replace(".","/")+".class"
+    val url = clazz.getResource(resName)
+    if url==null then
+      Left(s"resource $resName not found")
+    else
+      Using(url.openStream()) { stream => 
+        parse(stream.readAllBytes())
+      } match
+        case Failure(exception) => Left(exception.getMessage())
+        case Success(value) => value
+          
 }
