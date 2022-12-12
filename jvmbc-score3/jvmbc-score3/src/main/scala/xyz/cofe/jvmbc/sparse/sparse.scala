bf13266d8a14b31aeb91d7a57ec32b28ef87f1b5
Georgiy Kamnev <nt.gocha@gmail.com>
2022-12-12 12:34:23 +0500

    1

--- jvmbc-score3/jvmbc-score3/src/main/scala/xyz/cofe/jvmbc/sparse/sparse.scala
+++ jvmbc-score3/jvmbc-score3/src/main/scala/xyz/cofe/jvmbc/sparse/sparse.scala
@@ -1,5 +1,8 @@
 package xyz.cofe.jvmbc.sparse
 
+import scala.runtime.Tuples
+import scala.deriving._
+
 case class SPtr(string:String, value:Int):
   def hasTarget:Boolean = value >= 0 && value < string.length()
   def available:Int = 
@@ -19,3 +22,93 @@ case class SPtr(string:String, value:Int):
 
 trait Pattern[P]:
   def test(ptr:SPtr):Either[String,(P,SPtr)]
+  def alt(altPattern:Pattern[P]):Pattern[P] = 
+    val self = this
+    new Pattern[P] {
+      override def test(ptr: SPtr): Either[String, (P, SPtr)] = 
+        self.test(ptr).orElse( altPattern.test(ptr) )
+    }
+
+def matchz[T](str:String)(tok:String=>T):Pattern[T] = 
+  new Pattern[T] {
+    def test(ptr: SPtr): Either[String, (T, SPtr)] = 
+      if ptr.fetch(str.length()) == str 
+      then
+        Right((tok(str)), ptr + str.length())
+      else
+        Left(s"not match $str")
+  }
+
+case class Head[H,T](head:H, tail:T)
+case object HNil
+
+extension [A]( base:Pattern[A] )
+  def +[B]( next:Pattern[B] ):Head[Pattern[B],Pattern[A]] = 
+    Head(next,base)
+
+extension [A,B]( hlist:Head[Pattern[B],Pattern[A]] )
+  def tupled:Pattern[(A,B)] =
+    new Pattern[(A,B)] {
+      def test(ptr: SPtr): Either[String, ((A, B), SPtr)] = {
+        hlist.tail.test(ptr).flatMap { case(a,p) => 
+          hlist.head.test(p).map { case(b,p) => 
+            ( (a,b), p )
+          }
+        }
+      }
+    }
+
+extension [A,B]( from:Head[A,B] )
+  def +[C]( ptrn:Pattern[C] ):Head[Pattern[C],Head[A,B]] =
+    Head( ptrn, from )
+
+trait TupledPattern[A]:
+  type C
+  def tupled( from:A ):Pattern[C]
+
+object TupledPattern:
+  given [A,B]:TupledPattern[Head[Pattern[B],Pattern[A]]] with
+    type C = (A,B)
+    override def tupled(from: Head[Pattern[B], Pattern[A]]): Pattern[C] = 
+      new Pattern[C] {
+        def test(ptr: SPtr): Either[String, (C, SPtr)] = 
+          from.tail.test(ptr).flatMap { case (a,p) =>
+            from.head.test(p).map { case (b,p) => 
+              ( (a,b),p )
+            }
+          }
+      }
+
+  given x[A,B](using tp:TupledPattern[B]):TupledPattern[Head[Pattern[A],B]] with
+    type C = (tp.C,A)
+    override def tupled(from: Head[Pattern[A], B]): Pattern[C] = 
+      new Pattern[C] {
+        def test(ptr: SPtr): Either[String, (C, SPtr)] = 
+          tp.tupled(from.tail).test(ptr).flatMap { case (c,p) => 
+            from.head.test(p).map { case (a,p) => 
+              ( (c,a),p )
+            }
+          }
+      }
+
+extension [A](hl:A)(using tp:TupledPattern[A])
+  def tupled2:Pattern[tp.C] = new Pattern[tp.C] {
+    def test(ptr: SPtr): Either[String, (tp.C, SPtr)] = 
+      tp.tupled(hl).test(ptr)
+  }
+
+trait TupleFlatten[A <: Tuple]:
+  type  Out <: Tuple
+  def flatten( a:A ):Out
+
+object TupleFlatten:
+  given [A,B]: TupleFlatten[(A,B)] with
+    type Out = (A,B)
+    def flatten(a: (A, B)): Out = a
+
+  given [A,B,C]: TupleFlatten[((A,B),C)] with
+    type Out = (A,B,C)
+    def flatten(a: ((A, B), C)): Out = (a._1._1, a._1._2, a._2)
+
+extension [A,B,C](tupl:((A,B),C))(using fl:TupleFlatten[((A,B),C)])
+  def flat:fl.Out = fl.flatten(tupl)
--- jvmbc-score3/jvmbc-score3/src/test/scala/xyz/cofe/jvmbc/sparse/SParseTest.scala
+++ jvmbc-score3/jvmbc-score3/src/test/scala/xyz/cofe/jvmbc/sparse/SParseTest.scala
@@ -15,3 +15,78 @@ class SParseTest extends AnyFunSuite:
     val p1 = sptr-1
     assert( ! p1.hasTarget )
   }
+
+  case class Id(string:String)
+
+  test("alt test") {
+    val idPtrn = matchz("true")(Id)
+      .alt(matchz("false")(Id))
+      .alt(matchz("null")(Id))
+
+    assert( idPtrn.test(SPtr("true",0))==Right((Id("true")),SPtr("true",4)) )
+    println( idPtrn.test(SPtr(" false",1)) )
+    assert( idPtrn.test(SPtr(" false",1))==Right((Id("false")),SPtr(" false",6)) )
+    println( idPtrn.test(SPtr(" false",0)) )
+  }
+
+  case class Id2( first:Id, second:Id )
+
+  test("seq") {
+    val idPtrn1 = matchz("true")(Id)
+      .alt(matchz("false")(Id.apply _))
+      .alt(matchz("null")(Id))
+
+    val idPtrn2 = matchz("one")(Id)
+      .alt(matchz("two")(Id))
+      .alt(matchz("three")(Id))
+
+    val seq2HL = idPtrn1 + idPtrn2
+    val seq2Tpl = seq2HL.tupled
+    println( seq2Tpl.test(SPtr("trueone",0)) )
+    println( seq2Tpl.test(SPtr("truetwo",0)) )
+
+    val seq3HL = idPtrn1 + idPtrn2 + idPtrn1
+    seq3HL.tupled2.test(null).foreach{ x => 
+    }
+
+    val seq4HL = idPtrn1 + idPtrn2 + idPtrn1 + idPtrn2
+  }
+
+  case class BOOL(str:String)
+  case class DIGIT(str:String)
+  case class NUM(str:String)
+
+  test("seq 2") {
+    val src = SPtr("true1twoid-b",0)
+
+    val boolPtrn = matchz("true")(BOOL)
+      .alt(matchz("false")(BOOL))
+
+    val digitPtrn = matchz("1")(DIGIT)
+      .alt(matchz("2")(DIGIT))
+
+    println( (boolPtrn + digitPtrn).tupled2.test(src) )
+
+    val numPtrn = matchz("one")(NUM)
+      .alt(matchz("two")(NUM))
+
+    val ptrn3 = (boolPtrn + digitPtrn + numPtrn).tupled2
+    println( ptrn3.test(src) )
+
+    ptrn3.test(src).foreach { case (res,_) => 
+      val ((a:BOOL, b:DIGIT), c:NUM) = res
+      println( (a,b,c) )
+    }
+
+    // val idPtrn1 = matchz("id-a")(Id)
+    //   .alt(matchz("id-b")(Id))
+
+    // val ptrnHL = boolPtrn + digitPtrn + numPtrn + idPtrn1
+  }
+
+  test("flatten") {
+    val tup = (((10,"str"),false),'a')
+    val x = tup.last
+    val y = tup.flat
+    //val z = y.flat
+  }
