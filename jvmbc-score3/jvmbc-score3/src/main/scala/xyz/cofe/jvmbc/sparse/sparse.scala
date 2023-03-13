e44ff268bc9f78e555ea1517a952922112f40890
Georgiy Kamnev <nt.gocha@gmail.com>
2023-03-14 00:11:07 +0500

    text parse

--- jvmbc-score3/jvmbc-score3/src/main/scala/xyz/cofe/jvmbc/sparse/sparse.scala
+++ jvmbc-score3/jvmbc-score3/src/main/scala/xyz/cofe/jvmbc/sparse/sparse.scala
@@ -2,6 +2,7 @@ package xyz.cofe.jvmbc.sparse
 
 import scala.runtime.Tuples
 import scala.deriving._
+import scala.reflect.ClassTag
 
 case class SPtr(string:String, value:Int):
   def hasTarget:Boolean = value >= 0 && value < string.length()
@@ -21,32 +22,59 @@ case class SPtr(string:String, value:Int):
   def -(offsetValue:Int):SPtr = copy(value = value-offsetValue)
 
 trait Pattern[A]:
-  def test(ptr:SPtr):Either[String,(A,SPtr)]
+  def apply(ptr:SPtr):Either[String,(A,SPtr)]
+  def map[B](f:A=>B):Pattern[B] = 
+    val self = this
+    new Pattern[B] {
+      override def apply(ptr: SPtr): Either[String, (B, SPtr)] = 
+        self(ptr).map( (a,ptr) => (f(a),ptr) )
+    }
+
   def alt(altPattern:Pattern[A]):Pattern[A] = 
     val self = this
     new Pattern[A] {
-      override def test(ptr: SPtr): Either[String, (A, SPtr)] = 
-        self.test(ptr).orElse( altPattern.test(ptr) )
+      override def apply(ptr: SPtr): Either[String, (A, SPtr)] = 
+        self(ptr).orElse( altPattern(ptr) )
     }
+  def |(altPattern:Pattern[A]):Pattern[A] = alt(altPattern)
+
   def seq[B](p:Pattern[B]):Pattern[(A,B)] = 
     val base = this
     new Pattern[(A,B)] {
-      override def test(ptr: SPtr): Either[String, ((A, B), SPtr)] = 
-        base.test(ptr).flatMap { case (a,ptr2) => 
-          p.test(ptr2).map { case (b,ptr3) => 
+      override def apply(ptr: SPtr): Either[String, ((A, B), SPtr)] = 
+        base(ptr).flatMap { case (a,ptr2) => 
+          p(ptr2).map { case (b,ptr3) => 
             ( (a,b),ptr3 )
           }
         }
     }
   def +[B](p:Pattern[B]):Pattern[(A,B)] = seq[B](p)
-  def |(altPattern:Pattern[A]):Pattern[A] = alt(altPattern)
 
-def matchz[T](str:String)(tok:String=>T):Pattern[T] = 
-  new Pattern[T] {
-    def test(ptr: SPtr): Either[String, (T, SPtr)] = 
-      if ptr.fetch(str.length()) == str 
-      then
-        Right((tok(str)), ptr + str.length())
-      else
-        Left(s"not match $str")
-  }
+object Pattern:
+  def textMatch[T](str:String)(tok:String=>T):Pattern[T] = 
+    new Pattern[T] {
+      def apply(ptr: SPtr): Either[String, (T, SPtr)] = 
+        if ptr.fetch(str.length()) == str 
+        then
+          Right((tok(str)), ptr + str.length())
+        else
+          Left(s"not match $str")
+    }
+
+extension [A,B,C]( ptrn:Pattern[((A,B),C)] )
+  def map[Z]( f:(A,B,C)=>Z ):Pattern[Z] = ptrn.map { case((a,b),c) => f(a,b,c) }
+
+extension [A,B,C,D]( ptrn:Pattern[(((A,B),C),D)] )
+  def map[Z]( f:(A,B,C,D)=>Z ):Pattern[Z] = ptrn.map { case(((a,b),c),d) => f(a,b,c,d) }  
+
+extension [A,B,C,D,E]( ptrn:Pattern[((((A,B),C),D),E)] )
+  def map[Z]( f:(A,B,C,D,E)=>Z ):Pattern[Z] = ptrn.map { case((((a,b),c),d),e) => f(a,b,c,d,e) }
+
+extension [A,B,C,D,E,F]( ptrn:Pattern[(((((A,B),C),D),E),F)] )
+  def map[Z]( f:(A,B,C,D,E,F)=>Z ):Pattern[Z] = ptrn.map { case(((((a,b),c),d),e),f0) => f(a,b,c,d,e,f0) }
+
+extension [A,B,C,D,E,F,G]( ptrn:Pattern[((((((A,B),C),D),E),F),G)] )
+  def map[Z]( f:(A,B,C,D,E,F,G)=>Z ):Pattern[Z] = ptrn.map { case((((((a,b),c),d),e),f0),g) => f(a,b,c,d,e,f0,g) }
+
+extension [A,B,C,D,E,F,G,H]( ptrn:Pattern[(((((((A,B),C),D),E),F),G),H)] )
+  def map[Z]( f:(A,B,C,D,E,F,G,H)=>Z ):Pattern[Z] = ptrn.map { case(((((((a,b),c),d),e),f0),g),h) => f(a,b,c,d,e,f0,g,h) }
--- jvmbc-score3/jvmbc-score3/src/test/scala/xyz/cofe/jvmbc/sparse/SParseTest.scala
+++ jvmbc-score3/jvmbc-score3/src/test/scala/xyz/cofe/jvmbc/sparse/SParseTest.scala
@@ -2,6 +2,8 @@ package xyz.cofe.jvmbc.sparse
 
 import org.scalatest.funsuite.AnyFunSuite
 
+import xyz.cofe.jvmbc.sparse.Pattern.textMatch
+
 class SParseTest extends AnyFunSuite:
   test("SPtr") {
     val sptr = SPtr("abcde",0)
@@ -19,26 +21,26 @@ class SParseTest extends AnyFunSuite:
   case class Id(string:String)
 
   test("alt test") {
-    val idPtrn = matchz("true")(Id.apply _)
-      .alt(matchz("false")(Id.apply _))
-      .alt(matchz("null")(Id.apply _))
+    val idPtrn = textMatch("true")(Id.apply _)
+      .alt(textMatch("false")(Id.apply _))
+      .alt(textMatch("null")(Id.apply _))
 
-    assert( idPtrn.test(SPtr("true",0))==Right((Id("true")),SPtr("true",4)) )
-    println( idPtrn.test(SPtr(" false",1)) )
-    assert( idPtrn.test(SPtr(" false",1))==Right((Id("false")),SPtr(" false",6)) )
-    println( idPtrn.test(SPtr(" false",0)) )
+    assert( idPtrn(SPtr("true",0))==Right((Id("true")),SPtr("true",4)) )
+    println( idPtrn(SPtr(" false",1)) )
+    assert( idPtrn(SPtr(" false",1))==Right((Id("false")),SPtr(" false",6)) )
+    println( idPtrn(SPtr(" false",0)) )
   }
 
   case class Id2( first:Id, second:Id )
 
   test("seq") {
-    val idPtrn1 = matchz("true")(Id.apply _)
-      .alt(matchz("false")(Id.apply _))
-      .alt(matchz("null")(Id.apply _))
+    val idPtrn1 = textMatch("true")(Id.apply _)
+      .alt(textMatch("false")(Id.apply _))
+      .alt(textMatch("null")(Id.apply _))
 
-    val idPtrn2 = matchz("one")(Id.apply _)
-      .alt(matchz("two")(Id.apply _))
-      .alt(matchz("three")(Id.apply _))
+    val idPtrn2 = textMatch("one")(Id.apply _)
+      .alt(textMatch("two")(Id.apply _))
+      .alt(textMatch("three")(Id.apply _))
   }
 
   case class BOOL(str:String)
@@ -49,34 +51,21 @@ class SParseTest extends AnyFunSuite:
   test("seq 2") {
     val src = SPtr("true1anull",0)
 
-    val boolPtrn = matchz("true")(BOOL.apply _)
-      .alt(matchz("false")(BOOL.apply _))
+    val boolPtrn = textMatch("true")(BOOL.apply _)
+      .alt(textMatch("false")(BOOL.apply _))
 
-    val digitPtrn = matchz("1")(DIGIT.apply _)
-      .alt(matchz("2")(DIGIT.apply _))
+    val digitPtrn = textMatch("1")(DIGIT.apply _)
+      .alt(textMatch("2")(DIGIT.apply _))
 
     val letterPtrn = 
-      matchz("a")(LETTER.apply _) | matchz("b")(LETTER.apply _)
+      textMatch("a")(LETTER.apply _) | textMatch("b")(LETTER.apply _)
 
     val nullPtrn =
-      matchz("null")(NULL.apply _)
+      textMatch("null")(NULL.apply _)
 
     val p2 = boolPtrn + digitPtrn
     val p3 = p2 + letterPtrn
     val p4 = p3 + nullPtrn
 
-    (boolPtrn,digitPtrn,letterPtrn,nullPtrn)
+    p4.map( (a,b,c,d) => a )
   }
-
-  trait Pars[A]:
-    def pars(p:SPtr):Either[String,(A,SPtr)]
-
-  given Pars[EmptyTuple] with
-    def pars(p:SPtr):Either[String,(EmptyTuple,SPtr)] =
-      Left("!!EmptyTuple")
-
-  given [H:Pars, T <: Tuple : Pars]:Pars[H *: T] with
-    def pars(p:SPtr):Either[String,(H *: T,SPtr)] =
-      ???
-
-  
